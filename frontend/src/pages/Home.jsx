fa256688bc277777d1cf810540e8fe135b696b09
Kavinda Kiridana <kavindakiridena@gmail.com>
2024-12-29 21:21:34 +0530

    frontend part added

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { useAuth } from "../context/AuthContext";
+import BooksTable from "../components/home/BooksTable";
+//import BooksCard from "../components/home/BooksCard";
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
+  const { user, logout } = useAuth();
+  const userType = user?.userType;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/books")
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl">
+          Welcome, {user?.name ? user.name : "Guest"}!
+        </h1>
+        {/* <div>User Type: {user?.userType || "Not specified"}</div> */}
+        <div className="flex items-center gap-x-4">
+          {userType === "adminUser" && (
+            <Link to="/users">
+              <button className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded-lg">
+                See Users
+              </button>
+            </Link>
+          )}
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-lg"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Books List</h1>
+        {userType === "adminUser" && (
+          <Link to="/books/create">
+            <MdOutlineAddBox className="text-sky-800 text-4xl" />
+          </Link>
+        )}
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : showType === "table" ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
+      <div>
+        {" "}
+        {userType ? (
+          <p></p>
+        ) : (
+          <p>
+            only admins are allow the add, delete or modify the books details
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Home;
