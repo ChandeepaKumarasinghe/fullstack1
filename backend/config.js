720ded8f975cc599aa82e03a60449afc9b8d2c07
ChandeepaKumarasinghe <145914249+ChandeepaKumarasinghe@users.noreply.github.com>
2025-01-01 20:31:20 +0530

    aaaa
    aaa

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://kavindakiridena:UsGGXH0OjoB3oHDJ@cluster0.xlrfs.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0 ";
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
