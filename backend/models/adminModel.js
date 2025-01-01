141e644ae57dfa4fbf6df5aea7a73554cb7edb20
ChandeepaKumarasinghe <145914249+ChandeepaKumarasinghe@users.noreply.github.com>
2025-01-01 20:40:19 +0530

    complete site
    100% ok

---
+++ backend/models/adminModel.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    userType: {
+      type: String,
+      required: true, // Changed to required
+      enum: ["adminUser", "normalUser"], // Added enum to restrict values
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Pre-save middleware to hash password before saving
+adminSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) return next();
+
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password along with the salt
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to check password validity
+adminSchema.methods.isValidPassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export const Admin = mongoose.model("Admin", adminSchema);
